6d74372f1e24e0371effca321b8da69204e91577
zored <i@ro.gl>
2021-06-27 15:55:27 +0300

    [] Add counters

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-const index =
-  `<html><body><h1>Bro Deno</h1>This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.</body></html>`;
+let visits = 0;
+const deployedAt = new Date();
+
+const index = () =>
+  `<html><body><h1>Bro Deno</h1>This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.</body><code>Deployed at: ${deployedAt}\nVisits since deploy: ${visits++}</code></html>`;
+
 addEventListener("fetch", (event: FetchEvent) =>
   event.respondWith(
     new Response(index, {
@@ -7,3 +11,5 @@ addEventListener("fetch", (event: FetchEvent) =>
       headers: { "content-type": "text/html" },
     }),
   ));
+
+
