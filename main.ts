0576cd2c0200d1f5e10615c2f17db9b488cf0bf8
zored <i@ro.gl>
2021-06-27 16:41:24 +0300

    [master] Add upper

--- main.ts
+++ main.ts
@@ -1,43 +1,78 @@
 import { decode } from "https://deno.land/std@0.99.0/encoding/base64.ts";
+import { readAllSync } from "https://deno.land/std@0.99.0/io/util.ts";
 
-const notFound = "/404";
-const handlers: Record<string, Record<string, (e: FetchEvent, u: Url) => Response>> = {
+type Handler = Response | ((s: Server) => Response | Promise<Response>);
+type HandlersByMethodAndPath = Record<string, Record<string, Handler>>;
+
+const notFound: Handler = (s) => s.response({ body: `not found`, status: 404 });
+
+const handlers: HandlersByMethodAndPath = {
   GET: {
-    "/": (() => {
+    "/": ((): Handler => {
       const deployedAt = new Date();
       let visits = 0;
 
-      return () =>
-        new Response(
-          `<html><body><h1>Bro Deno</h1>This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.</body><br/><br/><code>Deployed at: ${deployedAt}\nVisits since deploy: ${visits++}</code></html>`,
-          {
-            status: 200,
-            headers: { "content-type": "text/html" },
-          },
-        );
+      return (s) =>
+        s.htmlResponse(`
+        <h1>Bro Deno</h1>
+        <p>
+          This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.
+        </p>
+        <code><pre>
+          Deployed at: ${deployedAt}
+          Visits since deploy: ${visits++}
+        </pre></code>
+      `);
     })(),
-    "/favicon.ico": () =>
-      new Response(
-        decode(
+    "/upper": async (s) =>
+      s.textResponse((await s.getRequestTextBody()).toUpperCase()),
+    "/favicon.ico": (s) =>
+      s.response({
+        body: decode(
           "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAMFBMVEU0OkArMjhobHEoPUPFEBIuO0L+AAC2FBZ2JyuNICOfGx7xAwTjCAlCNTvVDA1aLzQ3COjMAAAAVUlEQVQI12NgwAaCDSA0888GCItjn0szWGBJTVoGSCjWs8TleQCQYV95evdxkFT8Kpe0PLDi5WfKd4LUsN5zS1sKFolt8bwAZrCaGqNYJAgFDEpQAAAzmxafI4vZWwAAAABJRU5ErkJggg==",
         ),
-        {
-          status: 200,
-          headers: { "content-type": "image/x-icon" },
-        },
-      ),
-    [notFound]: (e: FetchEvent, u: URL) =>
-      new Response(`not found`, {
-        status: 404,
-        headers: { "content-type": "text/plain" },
+        mime: "image/x-icon",
       }),
   },
 };
 
-addEventListener("fetch", (event: FetchEvent) => {
-  const url = new URL(event.request.url);
-  const path = url.pathname;
-  const getResponse = handlers?.[event.request.method]?.[path] ??
-    handlers.GET[notFound];
-  event.respondWith(getResponse(event, url));
+addEventListener("fetch", async (event: FetchEvent) => {
+  const s = new Server(event.request);
+  const responseOrRetriever = handlers?.[event.request.method]?.[s.getPath()] ??
+    notFound;
+  const responseOrLazy = responseOrRetriever instanceof Response
+    ? responseOrRetriever
+    : responseOrRetriever(s);
+  const response = responseOrLazy instanceof Promise
+    ? await responseOrLazy
+    : responseOrLazy;
+  event.respondWith(response);
 });
+
+class Server {
+  constructor(public req: Request, private textDecoder = new TextDecoder()) {
+  }
+
+  async getRequestTextBody(): Promise<string> {
+    return this.textDecoder.decode(await Deno.readAll(this.req.body));
+  }
+
+  async htmlResponse(body: string) {
+    return this.response({
+      body: `<html><body>${body}</body></html>`,
+      mime: "text/html",
+    });
+  }
+
+  async textResponse(body: string) {
+    return this.response({ body });
+  }
+
+  response({ body = "" as BodyInit, mime = "text/plain", status = 200 }) {
+    return new Response(body, { status, headers: { "content-type": mime } });
+  }
+
+  getPath() {
+    return (new URL(this.req.url)).pathname;
+  }
+}
