6a29a909743b10a16623d2bdf84e5bd928ecdc59
zored <i@ro.gl>
2021-06-27 16:09:16 +0300

    [] Add favicon and 404 handling

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Deno Bro
+
 This is my [bro](https://bro.deno.dev) repository containing
 [`main.ts`](./main.ts) that is hosted on [Deno Deploy](https://deno.com/deploy).
 
 ## Dev
+
 - Types are generated with `deployctl types >  deployctl.d.ts`.
 - `main.ts` is auto-synced with Github on push.
--- main.ts
+++ main.ts
@@ -1,15 +1,41 @@
-let visits = 0;
-const deployedAt = new Date();
+import { decode } from "https://deno.land/std@0.99.0/encoding/base64.ts";
 
-const index = () =>
-  `<html><body><h1>Bro Deno</h1>This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.</body><br/><br/><code>Deployed at: ${deployedAt}\nVisits since deploy: ${visits++}</code></html>`;
+const notFound = "/404";
+const handlers: Record<string, Record<string, () => Response>> = {
+  GET: {
+    "/": (() => {
+      const deployedAt = new Date();
+      let visits = 0;
 
-addEventListener("fetch", (event: FetchEvent) =>
-  event.respondWith(
-    new Response(index(), {
-      status: 200,
-      headers: { "content-type": "text/html" },
-    }),
-  ));
+      return () =>
+        new Response(
+          `<html><body><h1>Bro Deno</h1>This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.</body><br/><br/><code>Deployed at: ${deployedAt}\nVisits since deploy: ${visits++}</code></html>`,
+          {
+            status: 200,
+            headers: { "content-type": "text/html" },
+          },
+        );
+    })(),
+    "/favicon.ico": () =>
+      new Response(
+        decode(
+          "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAMFBMVEU0OkArMjhobHEoPUPFEBIuO0L+AAC2FBZ2JyuNICOfGx7xAwTjCAlCNTvVDA1aLzQ3COjMAAAAVUlEQVQI12NgwAaCDSA0888GCItjn0szWGBJTVoGSCjWs8TleQCQYV95evdxkFT8Kpe0PLDi5WfKd4LUsN5zS1sKFolt8bwAZrCaGqNYJAgFDEpQAAAzmxafI4vZWwAAAABJRU5ErkJggg==",
+        ),
+        {
+          status: 200,
+          headers: { "content-type": "image/x-icon" },
+        },
+      ),
+    [notFound]: () =>
+      new Response("not found", {
+        status: 404,
+        headers: { "content-type": "text/plain" },
+      }),
+  },
+};
 
-
+addEventListener("fetch", (event: FetchEvent) => {
+  const getResponse = handlers?.[event.request.method]?.[event.request.url] ??
+    handlers.GET[notFound];
+  event.respondWith(getResponse());
+});
