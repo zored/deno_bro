e5ff16cdc682f9a480a4ca374fbf2f2ac83b0232
zored <i@ro.gl>
2021-06-27 15:56:32 +0300

    [] Nice formatting

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let visits = 0;
 const deployedAt = new Date();
 
 const index = () =>
-  `<html><body><h1>Bro Deno</h1>This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.</body><code>Deployed at: ${deployedAt}\nVisits since deploy: ${visits++}</code></html>`;
+  `<html><body><h1>Bro Deno</h1>This code is hosted by <a href="https://github.com/zored/deno_bro">GitHub repository</a>.</body><br/><br/><code>Deployed at: ${deployedAt}\nVisits since deploy: ${visits++}</code></html>`;
 
 addEventListener("fetch", (event: FetchEvent) =>
   event.respondWith(
